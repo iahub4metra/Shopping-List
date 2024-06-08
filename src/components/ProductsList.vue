c1c70c948a9b7481e60416714d92aa71c5b95ca6
iahub4metra <buhai.artem7@gmail.com>
2024-06-08 20:52:15 +0100

    done

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,8 +1,8 @@
 <template>
         <h2 class="list-title">{{categoryTitle}}</h2>
         <ul>
-            <li v-for="product in filteredProducts(categoryType)" :key="product.id" class="list-item">
-                <input type="checkbox" name="checkbox-item" id="checkbox">
+            <li v-for="product in products" :key="product.id" class="list-item">
+                <input type="checkbox" name="checkbox-item" id="checkbox" v-model="product.completed">
                 <checkmarkIcon />
                 <svg class="svg-icon">
                     <use xlink:href="#icon-checkmark"></use>
@@ -25,7 +25,4 @@ const emit = defineEmits(['remove'])
 const remove = (id)=>{
     emit('remove', id)
 }
-const filteredProducts = (type) => {
-    return props.products.filter(product => product.type === props.categoryType);
-  }
 </script>
